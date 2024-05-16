4df33aaca2bdfee3cac85cd8eb83429bbbb4ca29
Markus Haas <haas.markus@protonmail.com>
2024-05-16 15:00:46 +0200

    Add WalletSDK provider

--- app/src/main/java/org/ethereumhpone/messenger/di/AppModule.kt
+++ app/src/main/java/org/ethereumhpone/messenger/di/AppModule.kt
@@ -3,16 +3,51 @@ package org.ethereumhpone.messenger.di
 import android.app.Application
 import android.content.ContentResolver
 import android.content.Context
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.ethereumhpone.messenger.BuildConfig
+import org.ethereumphone.walletsdk.WalletSDK
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    fun chainToApiKey(networkName: String): String = when(networkName) {
+        "eth-mainnet" -> BuildConfig.ETHEREUM_API
+        "eth-sepolia" -> BuildConfig.SEPOLIA_API
+        "opt-mainnet" -> BuildConfig.OPTIMISM_API
+        "arb-mainnet" -> BuildConfig.ARBITRUM_API
+        "polygon-mainnet" -> BuildConfig.POLYGON_API
+        "base-mainnet" -> BuildConfig.BASE_API
+        "eth-goerli" -> BuildConfig.BASE_API
+        else -> ""
+    }
+
+    fun chainIdToName(chainId: Int): String = when(chainId) {
+        1 -> "eth-mainnet"
+        11155111 -> "eth-sepolia"
+        10 -> "opt-mainnet"
+        42161 -> "arb-mainnet"
+        137 -> "polygon-mainnet"
+        8453 -> "base-mainnet"
+        5 -> "eth-goerli"
+        else -> ""
+    }
+
+    fun chainIdToRPC(chainId: Int): String {
+        return "https://${chainIdToName(chainId)}.g.alchemy.com/v2/${chainToApiKey(chainIdToName(chainId))}"
+    }
 
     @Provides
     @Singleton
@@ -22,5 +57,26 @@ object AppModule {
     @Singleton
     fun provideContentResolver(@ApplicationContext context: Context): ContentResolver {
         return context.contentResolver
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeb3j(): Web3j {
+        return Web3j.build(HttpService(chainIdToRPC(1)))
+    }
+
+    @Provides
+    @Singleton
+    fun bindWalletSDK(
+        @ApplicationContext context: Context,
+    ): WalletSDK {
+        var walletSDK = WalletSDK(context)
+
+        runBlocking {
+            val currentChainId = walletSDK.getChainId()
+            walletSDK = WalletSDK(context, Web3j.build(HttpService(chainIdToRPC(currentChainId))))
+        }
+
+        return walletSDK
     }
 }
