8969ee87f3b88c8775038e75623a330c4b5e4022
Markus Haas <haas.markus@protonmail.com>
2024-05-17 11:05:38 +0200

    Add a good transition

--- app/src/main/java/org/ethereumhpone/messenger/navigation/MessagingNavHost.kt
+++ app/src/main/java/org/ethereumhpone/messenger/navigation/MessagingNavHost.kt
@@ -3,9 +3,15 @@ package org.ethereumhpone.messenger.navigation
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
@@ -31,6 +37,31 @@ fun MessagingNavHost(
         navController = navController,
         startDestination = startDestination,
         modifier = modifier,
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> fullWidth }, // Start from right
+                animationSpec = tween(300)
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { fullWidth -> -fullWidth }, // Exit to left
+                animationSpec = tween(300)
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> -fullWidth }, // Start from left
+                animationSpec = tween(300)
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { fullWidth -> fullWidth }, // Exit to right
+                animationSpec = tween(300)
+            )
+        }
+
     ){
 
 
