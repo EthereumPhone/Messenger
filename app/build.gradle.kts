4dcb2dc82cd5bc624b5891fbde9963d31c759ddd
Markus Haas <haas.markus@protonmail.com>
2024-06-15 19:17:00 +0200

    Update versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "org.ethereumhpone.messenger"
         minSdk = 34
         targetSdk = 34
-        versionCode = 1
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
