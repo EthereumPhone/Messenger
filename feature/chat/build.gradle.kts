02df1a13aa8a03dfa7ec9f9e6f17e80772021c79
Markus Haas <haas.markus@protonmail.com>
2024-06-17 21:18:01 +0200

    Update coil

--- feature/chat/build.gradle.kts
+++ feature/chat/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
 
 
 
-    implementation("io.coil-kt:coil-compose:2.1.0")
+    implementation(libs.coil.compose)
     implementation("com.github.EthereumPhone:ethOS-Component-Library:1a10060494")
 
     implementation(libs.androidx.navigation.common.ktx)
