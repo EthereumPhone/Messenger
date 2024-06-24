b75cd7f1a39d1a0063b9d76f4ceda398b5b5d620
Nicola-Ceornea <nicola.ceornea@hotmail.com>
2024-06-24 16:01:57 +0200

    Linting

--- app/src/main/java/org/ethereumhpone/messenger/ui/MessagingApp.kt
+++ app/src/main/java/org/ethereumhpone/messenger/ui/MessagingApp.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import org.ethereumhpone.messenger.navigation.MessagingNavHost
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessagingApp(
     threadId: Int? = null
