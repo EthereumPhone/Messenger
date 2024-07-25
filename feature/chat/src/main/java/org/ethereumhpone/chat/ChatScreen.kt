84f499f8837c64235d4ac098ce578207f8c59c54
Markus Haas <haas.markus@protonmail.com>
2024-07-25 16:04:41 +0200

    Add onContact click, fix modal height

--- feature/chat/src/main/java/org/ethereumhpone/chat/ChatScreen.kt
+++ feature/chat/src/main/java/org/ethereumhpone/chat/ChatScreen.kt
@@ -1,9 +1,11 @@
 package org.ethereumhpone.chat
 
+import android.content.Intent
 import android.net.Uri
+import android.provider.ContactsContract
 import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
@@ -13,7 +15,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -24,9 +25,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.exclude
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.navigationBars
@@ -39,15 +40,27 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material3.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.InsertPhoto
+import androidx.compose.material.icons.outlined.Mood
+import androidx.compose.material.icons.rounded.ArrowUpward
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -56,71 +69,52 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
-import org.ethereumhpone.chat.components.AssetPickerSheet
-import org.ethereumhpone.chat.components.InputSelector
-import org.ethereumhpone.chat.components.message.MessageItem
-import org.ethosmobile.components.library.theme.Colors
-import org.ethosmobile.components.library.theme.Fonts
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.outlined.AttachMoney
-import androidx.compose.material.icons.outlined.Call
-import androidx.compose.material.icons.outlined.Contacts
-import androidx.compose.material.icons.outlined.InsertPhoto
-import androidx.compose.material.icons.outlined.Mood
-import androidx.compose.material.icons.outlined.PermMedia
-import androidx.compose.material.icons.rounded.ArrowBackIosNew
-import androidx.compose.material.icons.rounded.ArrowUpward
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.zIndex
-import org.ethereumhpone.chat.components.FunctionalityNotAvailablePanel
-import org.ethereumhpone.chat.components.WalletSelector
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.Player
+import kotlinx.coroutines.launch
+import org.ethereumhpone.chat.components.AssetPickerSheet
 import org.ethereumhpone.chat.components.ChatHeader
-import org.ethereumhpone.chat.components.message.ComposablePosition
-import org.ethereumhpone.chat.components.ContactItem
 import org.ethereumhpone.chat.components.ContactSheet
 import org.ethereumhpone.chat.components.DetailSelector
+import org.ethereumhpone.chat.components.FunctionalityNotAvailablePanel
 import org.ethereumhpone.chat.components.GallerySheet
+import org.ethereumhpone.chat.components.InputSelector
 import org.ethereumhpone.chat.components.MediaSheet
 import org.ethereumhpone.chat.components.MembersSheet
-import org.ethereumhpone.chat.components.message.TxMessage
 import org.ethereumhpone.chat.components.TXSheet
+import org.ethereumhpone.chat.components.WalletSelector
 import org.ethereumhpone.chat.components.attachments.AttachmentRow
 import org.ethereumhpone.chat.components.customBlur
-import org.ethereumhpone.chat.components.makePhoneCall
+import org.ethereumhpone.chat.components.message.ComposablePosition
+import org.ethereumhpone.chat.components.message.MessageItem
+import org.ethereumhpone.chat.components.message.TxMessage
 import org.ethereumhpone.database.model.Message
 import org.ethereumhpone.database.model.Recipient
 import org.ethereumhpone.domain.model.Attachment
-import org.ethosmobile.components.library.core.ethOSIconButton
-import java.util.Collections.list
+import org.ethosmobile.components.library.theme.Colors
+import org.ethosmobile.components.library.theme.Fonts
 
 
 @Composable
@@ -157,6 +151,7 @@ fun ChatRoute(
         onFocusedMessageUpdate = chatViewModel::updatefocusedMessage,
         onPhoneClicked = chatViewModel::callPhone,
         onPlayVideo = mediaViewModel::playVideo,
+        onOpenContact = chatViewModel::onOpenContact
 
     )
 }
@@ -181,6 +176,7 @@ fun ChatScreen(
     onFocusedMessageUpdate: (Message) -> Unit,
     onPhoneClicked: () -> Unit,
     onPlayVideo: (Uri) -> Unit,
+    onOpenContact: () -> Unit
 ){
 
     
@@ -292,31 +288,32 @@ fun ChatScreen(
                         showModalSheet = true
                     },
                     onContactClick = {
-                        currentModalSelector = DetailSelector.CONTACT
-                        showModalSheet = true
+                        // Redirect to contacts app
+                        onOpenContact()
                     }
                 )
             }
 
-                Box(modifier = modifier
-                    .fillMaxSize()
-                    .customBlur(if (focusMode.value) 100f else 0f)
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onTap = {
-                                if (focusMode.value) {
-                                    //Toast.makeText(context,"focus tap press -kkkk",Toast.LENGTH_SHORT).show()
-                                    focusMode.value = false
+                Box(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .customBlur(if (focusMode.value) 100f else 0f)
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = {
+                                    if (focusMode.value) {
+                                        //Toast.makeText(context,"focus tap press -kkkk",Toast.LENGTH_SHORT).show()
+                                        focusMode.value = false
+                                    }
+                                },
+                                onLongPress = {
+                                    if (focusMode.value) {
+                                        //Toast.makeText(context,"focus tap press -kkkk",Toast.LENGTH_SHORT).show()
+                                        focusMode.value = false
+                                    }
                                 }
-                            },
-                            onLongPress = {
-                                if (focusMode.value) {
-                                    //Toast.makeText(context,"focus tap press -kkkk",Toast.LENGTH_SHORT).show()
-                                    focusMode.value = false
-                                }
-                            }
-                        )
-                    },
+                            )
+                        },
                 ){
 
                     Column(
@@ -667,7 +664,7 @@ fun ChatScreen(
                         animationSpec = tween(300),
                     ),
                     exit = fadeOut(
-                        animationSpec = tween(300,),
+                        animationSpec = tween(300),
                     ),
                 ){
                     if (focusedMessage != null) {
@@ -715,23 +712,28 @@ fun ChatScreen(
                     },
                     sheetState = modalAssetSheetState
                 ) {
-                    when(currentModalSelector){
-                        DetailSelector.CONTACT -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(LocalConfiguration.current.screenHeightDp.dp.times(0.7f)) // Set height to 70% of the screen
+                    ) {
+                        when(currentModalSelector){
+                            DetailSelector.CONTACT -> {
 
-                            ContactSheet(
-                                name = recipient?.getDisplayName() ?: "",
-                                image = recipient?.contact?.photoUri ?: "",
-                                ens = listOf(""),
-                                recipient = recipient,
-                            )
+                                ContactSheet(
+                                    name = recipient?.getDisplayName() ?: "",
+                                    image = recipient?.contact?.photoUri ?: "",
+                                    ens = listOf(""),
+                                    recipient = recipient,
+                                )
+                            }
+
+                            DetailSelector.MEDIA -> MediaSheet(messagesUiState)
+                            DetailSelector.MEMBERS -> MembersSheet()
+                            DetailSelector.TXS -> TXSheet(messagesUiState)
+                            DetailSelector.ASSET -> AssetPickerSheet()
                         }
-
-                        DetailSelector.MEDIA -> MediaSheet(messagesUiState)
-                        DetailSelector.MEMBERS -> MembersSheet()
-                        DetailSelector.TXS -> TXSheet(messagesUiState)
-                        DetailSelector.ASSET -> AssetPickerSheet()
                     }
-
                 }
             }
         }
@@ -802,8 +804,7 @@ fun SelectorExpanded(
             modifier = Modifier
                 .padding(top = 8.dp)
                 .clip(CircleShape)
-                .size(42.dp)
-            ,
+                .size(42.dp),
             enabled = true,
             onClick = {
                 onSelectorChange(InputSelector.EMOJI)
@@ -827,8 +828,7 @@ fun SelectorExpanded(
                 modifier = Modifier
                     .padding(top = 8.dp)
                     .clip(CircleShape)
-                    .size(42.dp)
-                ,
+                    .size(42.dp),
                 enabled = true,
                 onClick = {
                     onSelectorChange(InputSelector.WALLET)
@@ -854,8 +854,7 @@ fun SelectorExpanded(
             modifier = Modifier
                 .padding(top = 8.dp)
                 .clip(CircleShape)
-                .size(42.dp)
-            ,
+                .size(42.dp),
             enabled = true,
             onClick = {
                 onSelectorChange(InputSelector.PICTURE)
