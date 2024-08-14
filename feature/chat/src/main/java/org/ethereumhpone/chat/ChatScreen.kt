c1e8958f30a730ee3dfc936ef06a169a6e7b8f0c
emunsi <emunsi123@outlook.com>
2024-08-14 18:23:54 +0200

    modalsheet ui fix

--- feature/chat/src/main/java/org/ethereumhpone/chat/ChatScreen.kt
+++ feature/chat/src/main/java/org/ethereumhpone/chat/ChatScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.exclude
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -754,6 +755,7 @@ fun ChatScreen(
                     containerColor= Colors.BLACK,
                     contentColor= Colors.WHITE,
 
+                    modifier = Modifier.fillMaxHeight(0.95f),
                     onDismissRequest = {
                         scope.launch {
                             modalAssetSheetState.hide()
