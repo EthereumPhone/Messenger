dac9749f71f721daacb8943e45adaf63859950fb
Nicola-Matia Ceornea <nicola.ceornea@hotmail.com>
2024-09-23 20:21:02 +0200

    added page scrolling via buttons

--- feature/onboarding/src/main/java/org/ethereumphone/onboarding/OnboardingScreen.kt
+++ feature/onboarding/src/main/java/org/ethereumphone/onboarding/OnboardingScreen.kt
@@ -1,12 +1,17 @@
 package org.ethereumphone.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 
 @Composable
 fun OnboardingRoute() {
@@ -26,19 +31,38 @@ fun OnboardingScreen() {
 
 
     Column() {
-        HorizontalPager(pagerState) { }
+        HorizontalPager(
+            state = pagerState,
+            userScrollEnabled = false
+        ) {
 
+        }
 
-
+        val coroutineScope = rememberCoroutineScope()
 
         Row {
             if (pagerState.currentPage > 0) {
-                Text("Back")
+                Text(
+                    text = "Back",
+                    modifier = Modifier.clickable {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                        }
+                    }
+                )
             }
 
 
             if (pagerState.currentPage != pagerState.pageCount -1) {
-                Text("Next")
+                Text(
+                    text = "Next",
+                    modifier = Modifier.clickable {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                        }
+                    }
+
+                )
             }
 
         }
