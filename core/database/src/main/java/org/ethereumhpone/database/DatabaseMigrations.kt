773d580d37989dfa1d7f35617c767246ca8edb04
Nicola-Matia Ceornea <nicola.ceornea@hotmail.com>
2024-09-25 21:51:17 +0200

    simplified messageDB drop added mmspart drop

--- core/database/src/main/java/org/ethereumhpone/database/DatabaseMigrations.kt
+++ core/database/src/main/java/org/ethereumhpone/database/DatabaseMigrations.kt
@@ -30,39 +30,11 @@ val migration1To2 = object: Migration(1,2) {
         //migrate convo
         database.execSQL("ALTER TABLE conversation ADD COLUMN isUnknown INTEGER NOT NULL DEFAULT 0")
 
-        //migrate message:
+        //drop message:
         database.execSQL("DROP TABLE IF EXISTS message")
-        database.execSQL("""
-            CREATE TABLE IF NOT EXISTS message (
-                id INTEGER PRIMARY KEY NOT NULL,
-                threadId INTEGER NOT NULL,
-                contentId INTEGER NOT NULL,
-                address TEXT NOT NULL,
-                boxId INTEGER NOT NULL,
-                type TEXT NOT NULL,
-                date INTEGER NOT NULL,
-                dateSent INTEGER NOT NULL,
-                seen INTEGER NOT NULL,
-                read INTEGER NOT NULL,
-                locked INTEGER NOT NULL,
-                subId INTEGER NOT NULL,
-                body TEXT NOT NULL,
-                errorCode INTEGER NOT NULL,
-                deliveryStatus INTEGER NOT NULL,
-                attachmentTypeString TEXT NOT NULL,
-                attachmentType TEXT NOT NULL,
-                mmsDeliveryStatusString TEXT NOT NULL,
-                readReportString TEXT NOT NULL,
-                errorType INTEGER NOT NULL,
-                messageSize INTEGER NOT NULL,
-                messageType INTEGER NOT NULL,
-                mmsStatus INTEGER NOT NULL,
-                subject TEXT NOT NULL,
-                textContentType TEXT NOT NULL,
-                parts TEXT NOT NULL,
-                UNIQUE(threadId) ON CONFLICT IGNORE
-            )
-        """.trimIndent())
+
+        database.execSQL("DROP TABLE IF EXISTS mms_part")
+
 
         // migrate mmsPart
 
