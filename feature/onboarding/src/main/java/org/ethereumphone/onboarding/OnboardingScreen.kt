5d3bfd953dbd02e13124e73cc48d3acdf8a07306
Nicola-Matia Ceornea <nicola.ceornea@hotmail.com>
2024-10-09 23:08:17 +0200

    Finished Loading circle

--- feature/onboarding/src/main/java/org/ethereumphone/onboarding/OnboardingScreen.kt
+++ feature/onboarding/src/main/java/org/ethereumphone/onboarding/OnboardingScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Text
@@ -20,7 +22,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,6 +40,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
@@ -77,6 +86,9 @@ fun OnboardingScreen() {
                         }
                     )
                 }
+                1 -> {
+                    DegenLoadingCircle()
+                }
             }
         }
     }
@@ -119,6 +131,43 @@ private fun PagerContent(
     }
 }
 
+@Composable
+private fun DegenLoadingCircle() {
+
+    var activatedBox by remember { mutableIntStateOf(0) }
+
+    val alignments = listOf(
+        Alignment.CenterStart,
+        Alignment.TopStart,
+        Alignment.TopCenter,
+        Alignment.TopEnd,
+        Alignment.CenterEnd,
+        Alignment.BottomEnd,
+        Alignment.BottomCenter,
+        Alignment.BottomStart
+    )
+
+    Box(Modifier.size(100.dp)) {
+        Box(Modifier.align(Alignment.Center).size(15.dp).background(Color.DarkGray))
+        alignments.forEachIndexed { index, alignment ->
+
+            Box(
+                Modifier
+                    .align(alignment)
+                    .size(15.dp)
+                    .background(if (activatedBox == index) Color.White else Color.DarkGray)
+            )
+        }
+    }
+
+    LaunchedEffect(null) {
+        while (true) {
+            delay(100L)
+            activatedBox = (activatedBox + 1) % alignments.size
+        }
+    }
+}
+
 enum class OnboardingPageContent(
     val title: String,
     val description: String
@@ -137,6 +186,15 @@ enum class OnboardingPageContent(
 fun previewOnboarding() {
     Column(Modifier.background(Color.Black)) {
         OnboardingScreen()
+
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun previewLoadingCircle() {
+    Column(Modifier.background(Color.Black)) {
+        DegenLoadingCircle()
 
     }
 }
