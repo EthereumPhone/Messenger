9a8a20de37100844f3671a2901f73d44a8717c08
Markus Haas <haas.markus@protonmail.com>
2025-12-04 08:26:20 +0100

    Bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "org.ethereumhpone.messenger"
         minSdk = 34
         targetSdk = 34
-        versionCode = 305
+        versionCode = 306
         versionName = versionCode.toString()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
